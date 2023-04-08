91ddfbcf73ac03c1907b38417244c2f1a6f9fffc
nobiduke <lmjones647@gmail.com>
2023-04-08 16:20:45 -0400

    updated shop

--- UI/index.js
+++ UI/index.js
@@ -1,46 +1,55 @@
-let bitGrid = document.getElementById('bit-grid');
+
+
+var bitGrid = document.getElementById('bit-grid');
+var bitAmount = document.getElementById('shop-bit-amount');
+const BITDIM = 20;
+const BIT_OFF = '#AA1111';
+const BIT_ON = '#00CC00';
+
+// intialize bit grid
 for(let i = 0; i < 32; i++){
     let bitElem = document.createElement('div');
-    bitElem.style.width = '20px';
-    bitElem.style.height = '20px';
-    bitElem.style.backgroundColor = '#AA1111';
-    bitElem.style.marginTop = '20px';
-    bitElem.style.zIndex = '10';
+    bitElem.style.width = `${BITDIM}px`;
+    bitElem.style.height = `${BITDIM}px`;
+    bitElem.style.backgroundColor = BIT_OFF;
     bitElem.id = `bit-${31-i}`;
 
+    // add each bit to bit grid 
+    // (most sig -> least sig)
     bitGrid.appendChild(bitElem);
 }
 
 let num = 0;
 
-function updateBits(strNum){
-    let i = 0
-    for(const node of document.getElementById('bit-grid').children){
-        
-        if (strNum[i] == '0'){
-            node.style.backgroundColor = '#AA1111';
-        } else{
-            node.style.backgroundColor = '#00CC00';
-        }
-        i++;
-    }
-}
-
-let frame = 0;
-function looper(d){
-    frame++;
-    if (frame % 64 != 0){
-        requestAnimationFrame(looper);
-        return;
-    }
+function updateBits(num){
     let strNum = num.toString(2);
     let zeros = "";
     for(let i = 0; i < 32-strNum.length; i++){
         zeros+="0";
     }
     strNum = zeros+strNum;
-    updateBits(strNum);
-    num++;
+    let i = 0
+    for(const node of document.getElementById('bit-grid').children){
+        
+        if (strNum[i] == '0'){
+            node.style.backgroundColor = BIT_OFF;
+        } else{
+            node.style.backgroundColor = BIT_ON;
+        }
+        i++;
+    }
+}
+
+let frame = 0;
+let delay = 64;
+function looper(d){
+    if (frame % delay == 0){
+        num+=1;
+        updateBits(num);
+    }
+    
+    frame++;
+    bitAmount.innerHTML = num;
     window.requestAnimationFrame(looper);
 }
 
